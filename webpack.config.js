635beda3cf7ec9188be6dcf1f02cee534e06f397
Piotr Nowinski <pitred007git@gmail.com>
2019-06-09 01:37:59 +0200

    Changing configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,35 @@
 const path = require('path');
 
 //webpack.config.js
-module.exports = {
-   mode: 'development',
-   entry: './src/index.js',
-   output: {
-      path: path.resolve(__dirname, 'build'),
-      filename: 'app.bundle.js'
-   },
-   module: {
-      rules: [
-         {
-            test: /\.js$/,
-            loader: 'babel-loader'
-         },
-         {
-            test: /\.css$/,
-            use: [
-               { loader: 'style-loader' },
-               {
-                  loader: 'css-loader',
-                  options: {
-                     modules: true
+module.exports = env => {
+   const environment = env || 'production';
+   return {
+      mode: 'environment',
+      entry: './src/index.js',
+      output: {
+         path: path.resolve(__dirname, 'build'),
+         filename: 'app.' + environment + '.bundle.js'
+      },
+      module: {
+         rules: [
+            {
+               test: /\.js$/,
+               exclude: /(node_modules|bower_components)/,
+               loader: 'babel-loader'
+            },
+            {
+               test: /\.css$/,
+               use: [
+                  { loader: 'style-loader' },
+                  {
+                     loader: 'css-loader',
+                     options: {
+                        modules: true
+                     }
                   }
-               }
-            ]
-         }
-      ]
-   }
+               ]
+            }
+         ]
+      }
+   };
 };
